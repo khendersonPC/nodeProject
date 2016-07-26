6e0722c44a43b32e1e78c1f120de4ddf222b822c
khendersonPC <khenderson@pcschools.us>
2016-07-26 14:30:54 -0600

    ignore bin files
    bin/

---
+++ nodeProject/Model/DataNode.h
@@ -0,0 +1,14 @@
+//
+//  DataNode.hpp
+//  nodeProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef DataNode_h
+#define DataNode_h
+
+#include <stdio.h>
+
+#endif /* DataNode_h */
---
+++ nodeProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nodeProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+
+    
+    return 0;
+}
