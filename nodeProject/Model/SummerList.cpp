eabf990a9bfbeb2152bceadea01cb769e87ac295
Henrichsen <cody.henrichsen@740s-pl-120.local>
2016-07-27 14:41:12 -0600

    added add method to summerList cpp

--- nodeProject/Model/SummerList.cpp
+++ nodeProject/Model/SummerList.cpp
@@ -35,8 +35,7 @@ void SummerList<Type> :: addAtIndex(int index, Type data)
     }
     else if(index == size - 1)
     {
-        end->setNodePointer(newNode);
-        end = newNode;
+        add(data);
     }
     else
     {
@@ -54,4 +53,13 @@ void SummerList<Type> :: addAtIndex(int index, Type data)
         //setting indexPointer to point to newNode address
   
     }
+}
+
+template <class Type>
+void SummerList<Type> :: add(Type data)
+{
+    DataNode<Type> *newNode = new DataNode<Type>(data);
+    
+    end->setNodePointer(newNode);
+    end = newNode;
 }
