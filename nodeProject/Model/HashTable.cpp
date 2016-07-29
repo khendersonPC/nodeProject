e336416054082c7452117db50e2fda2c329b2990
Henrichsen <cody.henrichsen@740s-pl-120.local>
2016-07-29 10:53:52 -0600

    wrote HashTable.cpp

--- nodeProject/Model/HashTable.cpp
+++ nodeProject/Model/HashTable.cpp
@@ -7,3 +7,34 @@
 //
 
 #include "HashTable.hpp"
+
+
+template<class Type>
+
+HashTable<Type> :: HashTable()
+{
+    
+    
+    this->capacity = 101;
+    
+    this->size = 0;
+    
+    this->efficiencyPercentage = 0.66;
+    
+    
+    this->front = new HashNode<Type>();
+    
+    HashNode<Type> *currentEnd = front;
+
+    for(int index = 1; index < capacity; index ++)
+    {
+        HashNode<Type> *next = new HashNode<Type>();
+        currentEnd->setNode(next);
+        currentEnd = next;
+    }//end loop to create the 1st array of nodes for storage
+    
+}
+
+
+
+
